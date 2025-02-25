219edd01675d38df1664260e0cc1c5dc03f3b16c
azizfrdev <azizbekj948@gmail.com>
2025-02-25 13:34:42 +0500

    Footer + Header responsive

--- hayat/src/components/Burger.jsx
+++ hayat/src/components/Burger.jsx
@@ -12,6 +12,10 @@ const App = () => {
     setMenuOpen((prev) => !prev);
   };
 
+  const closeMenu = () => {
+    setMenuOpen(false)
+  }
+
   return (
     <div className="app">
       <div className="header2">
@@ -23,43 +27,43 @@ const App = () => {
             <div className="nav__inner">
               <ul className="nav__list">
                 <li>
-                  <Link to="/about" className="nav-link">
+                  <Link to="/about" className="nav-link" onClick={closeMenu}>
                     {t("about")}
                   </Link>
                 </li>
                 <li>
-                  <Link to="/services" className="nav-link">
+                  <Link to="/services" className="nav-link" onClick={closeMenu}>
                     {t("services")}
                   </Link>
                 </li>
                 <li>
-                  <Link to="/doctors" className="nav-link">
+                  <Link to="/doctors" className="nav-link" onClick={closeMenu}>
                     {t("doctors")}
                   </Link>
                 </li>
                 <li>
-                  <Link to="/news" className="nav-link">
+                  <Link to="/news" className="nav-link" onClick={closeMenu}>
                     {t("news")}
                   </Link>
                 </li>
                 <li>
-                  <Link to="/team" className="nav-link">
+                  <Link to="/team" className="nav-link" onClick={closeMenu}>
                     {t("team")}
                   </Link>
                 </li>
                 <li>
-                  <Link to="/media" className="nav-link">
+                  <Link to="/media" className="nav-link" onClick={closeMenu}>
                     {t("media")}
                   </Link>
                 </li>
                 <li>
-                  <Link to="/contact" className="nav-link">
+                  <Link to="/contact" className="nav-link" onClick={closeMenu}>
                     {t("contact")}
                   </Link>
                 </li>
               </ul>
               <BasicButtonExample />
-              <Link to="/results" className="nav__link-know">
+              <Link to="/results" className="nav__link-know" onClick={closeMenu}>
                 {t("results")}
               </Link>
             </div>
--- hayat/src/lang/LangButton.jsx
+++ hayat/src/lang/LangButton.jsx
@@ -10,14 +10,14 @@ function BasicButtonExample() {
 
   return (
     <ul className="lang-list">
-      <li className="lang-list__item" onClick={() => changeLanguage("uz")} style={{ cursor: "pointer" }}>
-        <p className="lang-list__link" style={{ width: 26 }}> UZ </p>
+      <li className="lang-list__item" onClick={() => changeLanguage("en")} style={{ cursor: "pointer" }}>
+        <p className="lang-list__link" style={{ width: 26 }}> EN </p>
       </li>
       <li className="lang-list__item" onClick={() => changeLanguage("ru")} style={{ cursor: "pointer" }}>
         <p className="lang-list__link"  style={{ width: 26 }}> Ру </p>
       </li>
-      <li className="lang-list__item" onClick={() => changeLanguage("en")} style={{ cursor: "pointer" }}>
-        <p className="lang-list__link" style={{ width: 26 }}> EN </p>
+      <li className="lang-list__item" onClick={() => changeLanguage("uz")} style={{ cursor: "pointer" }}>
+        <p className="lang-list__link" style={{ width: 26 }}> UZ </p>
       </li>
     </ul>
   );
