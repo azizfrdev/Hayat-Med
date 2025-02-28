95ce2a8324558ebd8a013d8ec6e40374aea8b9d5
azizfrdev <azizbekj948@gmail.com>
2025-02-28 17:49:06 +0500

    Doctors fixed

--- hayat/src/components/Doctor.jsx
+++ hayat/src/components/Doctor.jsx
@@ -1,15 +1,18 @@
 import React, { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
 import axios from "axios";
+import "../assets/css/Doctor.css";
 
-const api = 'https://hayatm.onrender.com';
+const api = "https://hayatm.onrender.com";
 
 const DoctorsCarousel = () => {
+  const { t } = useTranslation();
   const [doctors, setDoctors] = useState([]);
 
   const fetchDoctors = async () => {
     try {
       const response = await axios.get(`${api}/doctors`);
-      console.log("Fetched Doctors:", response.data.doctors); 
+      console.log("Fetched Doctors:", response.data.doctors);
       setDoctors(response.data.doctors);
     } catch (error) {
       console.error("Failed to fetch doctors:", error);
@@ -21,41 +24,46 @@ const DoctorsCarousel = () => {
   }, []);
 
   return (
-    <div className="p-4">
-      <h2 className="text-2xl font-bold mb-4">Our Doctors</h2>
-      {doctors.length > 0 ? (
-        <div
-          key={doctors.length}
-          spaceBetween={30}
-          slidesPerView={3}
-          navigation
-          pagination={{ clickable: true }}
-          autoplay={{ delay: 3000 }}
-          loop={true}
-          breakpoints={{
-            1024: { slidesPerView: 3 },
-            768: { slidesPerView: 2 },
-            0: { slidesPerView: 1 },
-          }}
-        >
-          {doctors.map((doctor) => (
-            <div key={doctor._id}>
-              <div className="p-4 bg-white rounded-lg shadow-md">
-                <img
-                  src={doctor.image}
-                  alt={doctor.en_name}
-                  className="w-full h-64 object-cover rounded-lg mb-4"
-                />
-                <h3 className="text-xl font-semibold">{doctor.en_name}</h3>
-                <p className="text-gray-500">{doctor.en_position}</p>
+    <>
+      <div className="specialist">
+        <h2 className="title service__title">{t("ourdoctors")}</h2>
+        {doctors.length > 0 ? (
+          <div className="container specialist__intro">
+            <div className="specialist__ui-slider-container">
+              <div className="specialist__arrows-container">
+                <div className="swiper specialist__slider-card specialist-slider--start swiper-container-initialized swiper-container-horizontal">
+                  <div className="swiper-wrapper">
+                    {doctors.map((doctor) => (
+                      <div key={doctor._id}>
+                        <div className="p-4 relative-box bg-white rounded-lg shadow-md">
+                          <div className="guy">
+                            <img
+                              src={doctor.image}
+                              alt={doctor.en_name}
+                              className="w-full h-64 object-cover rounded-lg mb-4"
+                            />
+                          </div>
+                          <div className="guy-info">
+                            <h3 className="guy-info__name">
+                              {doctor.en_name}
+                            </h3>
+                            <p className="guy-info__position">
+                              {doctor.en_position}
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </div>
               </div>
             </div>
-          ))}
-        </div>
-      ) : (
-        <p>No doctors available</p>
-      )}
-    </div>
+          </div>
+        ) : (
+          <p>No doctors available</p>
+        )}
+      </div>
+    </>
   );
 };
 
